243e9a65fe51361c844c04b3e59226ea7e41705e
Vlasov <bboysmo3@gmail.com>
2023-11-25 20:43:53 +0200

    create title logo and styles

--- src/App.js
+++ src/App.js
@@ -2,25 +2,59 @@ import React from 'react';
 import owl1 from './images/owl1.svg'
 
 export default function App() {
-  return (
-    <>
-        <nav className='navigation'>
-            <div className='navigation__block limited__container'>
-                <div className='navigation__content content__navigation'>
-                    <div className='content__navigation-logo'>
-                        <img src={ owl1 } alt='Logo' />
+
+
+    return (
+        <>
+            <header className='header'>
+                <div className='header__block limited__container'>
+                    <div className='header__content navigation'>
+                        <nav className='navigation__block'>
+                            <div className='navigation__content content__navigation'>
+
+                                <div className='content__navigation-logo'>
+                                    <img src={ owl1 } alt='Logo' />
+                                </div>
+
+                                <div className='content__navigation-items items'>
+                                    {/*Сделать все нижние блоки ссылками на компоненты, которые будут переносить на другую страницу*/ }
+                                    <a href='../public/index.html' className='items__link'>Home</a>
+                                    <a href='../public/index.html' className='items__link'>About Me</a>
+                                    <a href='../public/index.html' className='items__link'>Skills</a>
+                                    <a href='../public/index.html' className='items__link'>My works</a>
+                                    <a href='../public/index.html' className='items__link'>Contacts</a>
+                                </div>
+
+                                <div className='content__navigation-burger burger'>
+                                    <div className='burger-item'></div>
+                                </div>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </header>
 
-        <main>
-            Main
-        </main>
+            <main className='main'>
+                <div className='main__block limited__container'>
+                    <div className='main__content'>
+                        <div className='main__stamp stamp'>
+                            <div className='stamp__img'>
+                                <img src={ owl1 } alt='Logo' />
+                            </div>
+                        </div>
 
-        <footer>
-            Footer
-        </footer>
-    </>
-  );
+                        <div className='main__info'>
+                            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet dolor doloribus, eaque hic molestias nulla possimus? Adipisci amet aspernatur consectetur dolore dolorem doloremque, ea eaque id iusto labore laboriosam laudantium modi necessitatibus nemo omnis quaerat qui quia quisquam, quod repellat, repellendus sed soluta? Expedita, non quidem? Alias beatae ducimus eligendi et excepturi, exercitationem expedita facere fugiat hic ipsa ipsum, laboriosam laudantium nobis porro possimus quae quasi quod ratione reiciendis saepe ullam vitae, voluptatibus. Architecto asperiores consequuntur, corporis cumque cupiditate dignissimos dolore doloribus error et explicabo fuga fugit incidunt ipsa ipsum itaque, laboriosam maxime minima molestiae molestias nulla officiis placeat quaerat quasi quia quis, quisquam repudiandae sequi tempora temporibus velit? Alias architecto cupiditate dignissimos dolorum in nemo nostrum officia qui veritatis vitae! Eius, eos, necessitatibus. Consequatur distinctio fuga ipsum laudantium, modi natus non ullam ut voluptatum! Deleniti, ipsam, vel! Beatae deleniti expedita facilis id odio. Consequuntur enim illo perferendis quo quod, ratione repellat unde! Accusantium asperiores aspernatur consectetur corporis culpa, delectus doloribus dolorum eos expedita impedit in incidunt ipsam iure iusto mollitia numquam pariatur reprehenderit rerum similique voluptate? Animi dicta dignissimos dolorem dolorum eaque eveniet excepturi explicabo facere id labore, omnis provident quisquam recusandae sequi sit veritatis voluptatem? Alias aliquid dicta dolorum earum est, ex ipsum natus nisi optio, quam quibusdam recusandae sit voluptatum. Aliquid, at doloribus ea eaque enim magni necessitatibus neque nihil nobis porro provident quaerat quam recusandae reprehenderit, ullam. Ad consequatur cupiditate doloremque illo magni numquam possimus quo similique tempore vel. Amet dignissimos exercitationem hic ipsum, iure mollitia nihil, officiis perferendis possimus quos rem repudiandae rerum vero! Ab aperiam id sunt. Cum dolorum est et facere facilis fuga mollitia soluta suscipit ullam. Adipisci consequuntur, corporis ducimus error magnam modi molestiae necessitatibus praesentium, provident ratione reprehenderit tempore! Atque, dignissimos distinctio doloremque eos fuga, minus natus necessitatibus, odio rerum similique sit suscipit velit? A accusamus alias aliquid aperiam aspernatur assumenda consequatur cum deleniti dolorum eaque eius eligendi eos eum eveniet, explicabo facere hic id, iste laboriosam libero magni minima modi molestiae nulla optio possimus quae quas qui sapiente sint sunt temporibus vel velit. Alias amet aperiam at atque commodi culpa cum dolorum ducimus eligendi enim illo incidunt inventore ipsam iure magnam, maxime modi, mollitia odio odit quae saepe suscipit tempora vel! A aliquam delectus eius eos fuga impedit laboriosam laborum maxime nesciunt quae quam quos rem, sit tempora tempore ullam ut! Asperiores aspernatur assumenda at aut consectetur consequatur delectus deserunt dolorem dolores ea ex facilis, fugit iure iusto maiores modi nemo neque numquam odio perferendis placeat praesentium quaerat quam quas quis, quisquam quod quos rem tempora veritatis. Accusantium beatae dolor doloremque dolores est minus nobis optio provident quia, quisquam reiciendis repellat reprehenderit soluta! A adipisci beatae commodi consequuntur culpa dicta ducimus eligendi est facere fugit hic id laudantium molestiae neque nulla pariatur perferendis possimus quaerat quas quibusdam quis quo recusandae repudiandae sapiente sequi, voluptas voluptatum. Corporis culpa delectus dolore dolores facilis, hic numquam reiciendis tempore! Alias asperiores debitis dolores ex excepturi facere natus odio possimus ratione voluptates. Et excepturi facilis, id incidunt, iure necessitatibus nostrum obcaecati odio perferendis provident quam rem sequi sunt? Accusantium corporis eaque natus, obcaecati, officiis qui quia quo quod quos ratione voluptatem, voluptatum? Fugiat hic id labore, numquam quod voluptates voluptatum. Atque, dolore fugiat maxime modi necessitatibus odio, perspiciatis quae sequi, sunt temporibus ullam veritatis. Doloremque impedit molestiae quam quasi? Accusamus accusantium blanditiis culpa cupiditate deserunt, dolorem doloribus earum eius eos ex excepturi expedita incidunt ipsum iste laborum maxime natus necessitatibus nisi numquam optio porro possimus quam repudiandae sed similique totam unde, vero? Accusantium beatae consectetur, culpa cupiditate deserunt eaque enim laudantium minima neque nostrum obcaecati odit officia, provident quo repellendus vel voluptatem. Ab alias architecto commodi fugit libero nostrum obcaecati, pariatur vero voluptatum? Ab autem beatae blanditiis dignissimos eaque excepturi laborum magnam neque pariatur porro quis, rerum sapiente sint vero voluptate. Ad asperiores error ex harum hic, itaque magnam omnis reiciendis, sed sit, totam ut voluptatem voluptates. Asperiores harum numquam pariatur, quasi ullam vel vitae voluptatibus? Accusantium dignissimos distinctio error id laboriosam molestias perspiciatis quam quas saepe similique? A accusamus animi architecto delectus distinctio dolore dolores eaque eos, et facere impedit nemo nobis numquam odit praesentium ratione, rem sequi tempora tempore voluptate? Aliquam at consequuntur cum delectus dolorum eius expedita, fugiat fugit illo impedit in ipsa maiores minus modi nemo nihil nobis non nostrum nulla numquam omnis praesentium quibusdam quisquam repellendus sed tempore veritatis? Aliquid aut, commodi ducimus error expedita fuga harum hic nulla numquam perferendis placeat possimus quasi quia recusandae repellat repellendus sit. A accusantium adipisci aliquid at consequatur corporis cumque deleniti ex facere, in ipsam laborum maxime mollitia officiis possimus, quam sit suscipit tenetur unde voluptatibus. Blanditiis eum explicabo ipsa laboriosam molestias non, officia rem suscipit tempore? A accusamus accusantium alias aut dolorum eum eveniet laudantium modi nam necessitatibus nobis nulla pariatur porro, quam quas quis quo quod rem sed sint temporibus totam voluptatibus? Delectus deleniti dolore eaque incidunt magnam magni minus molestias neque possimus ullam. A amet at autem commodi consequuntur deleniti distinctio dolor eos ex facere hic illo laborum magni minima molestias nisi numquam obcaecati pariatur perferendis porro praesentium ratione rerum similique sint soluta, sunt tempore temporibus tenetur ullam, unde ut vel velit voluptate. Aperiam cum delectus illo maiores molestias neque nostrum officiis reiciendis rerum suscipit. Aut dolores doloribus dolorum ducimus et eveniet harum id iure nesciunt, non nulla officiis omnis qui. Consequuntur error excepturi explicabo illo libero, officiis quia tempore velit. Aliquam aliquid amet animi aspernatur, assumenda at commodi consequatur consequuntur cumque cupiditate deserunt dicta distinctio doloremque ducimus eius error esse eum exercitationem fuga impedit magnam maxime modi nobis, non numquam officia optio perferendis praesentium quae quas saepe ut veritatis vitae? Atque consequatur ea earum esse est exercitationem expedita harum sit! Alias aspernatur cumque error velit voluptatum! Adipisci animi asperiores autem cum debitis dolorum earum illo inventore molestias nemo nostrum obcaecati odit quidem quo, quos sint tempore temporibus. Ad commodi cum eos error facilis iusto laboriosam nobis, numquam perferendis quo recusandae reiciendis repellat repellendus saepe tempora ullam veritatis voluptas voluptate.</h1>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <footer className='footer'>
+                <div className='footer__block limited__container'>
+                    <div className='footer__content'>Footer</div>
+                </div>
+            </footer>
+        </>
+    );
 }
